f227b9a2dbe600430ab7e776eb72b9edc6e2aad4
Meghana <meghanan114@gmail.com>
2019-07-21 21:03:24 +0530

    First Commit
    Uploading All Files

---
+++ MyCode/src/FirstProgram.java
@@ -0,0 +1,40 @@
+
+public class FirstProgram 
+{
+	public static void main(String args[]) 
+	{
+		for (int i = 1; i <= 1; i++) 
+		{
+			for (int j = 1; j <= 1; j++) 
+			{
+				char a=65;
+				System.out.print(a);
+			}
+		}
+		System.out.println();
+		for (int i = 2; i <= 2; i++) 
+		{
+			for (int j = 3; j <= 3; j++) 
+			{
+				char a=65;
+				char b=66;
+				System.out.print(a);
+				System.out.print(b);
+			}
+		}
+		System.out.println();
+		for (int i = 3; i <= 3; i++) 
+		{
+			for (int j = 3; j <= 3; j++) 
+			{
+				char a=65;
+				char b=66;
+				char c=67;
+				System.out.print(a);
+				System.out.print(b);
+				System.out.print(c);
+			}
+		}
+		System.out.println();
+	}
+}
